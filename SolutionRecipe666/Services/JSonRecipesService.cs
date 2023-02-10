2b415d18ced9f5dfd6e9020cb3a9925c08c7dd8d
Frederic Colin <Frederic.Colin@equadex.net>
2023-02-10 16:28:22 +0100

    End for JsonRecipesService

--- SolutionRecipe666/Services/JSonRecipesService.cs
+++ SolutionRecipe666/Services/JSonRecipesService.cs
@@ -1,4 +1,5 @@
 ﻿using DataContracts;
+using Newtonsoft.Json;
 using ServicesContracts;
 using System;
 using System.Collections.Generic;
@@ -12,8 +13,8 @@ namespace Services
     {
         public override List<Recipe> GetAll()
         {
-            return null;
-            
+            var serializedString = File.ReadAllText("recipes.json");
+            return JsonConvert.DeserializeObject<List<Recipe>>(serializedString);   
         }
     }
 }
