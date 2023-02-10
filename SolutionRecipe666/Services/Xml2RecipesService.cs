e4fb1b51d4e3f29f5e81d48d7b1923a660e68915
Frederic Colin <Frederic.Colin@equadex.net>
2023-02-10 15:25:37 +0100

    Xml2RecipesService

---
+++ SolutionRecipe666/Services/Xml2RecipesService.cs
@@ -0,0 +1,24 @@
+﻿using DataContracts;
+using ServicesContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Services
+{
+    public class Xml2RecipesService : AbtractRecipeService
+    {
+        public override List<Recipe> GetAll()
+        {
+            var xdoc = XDocument.Load("recipes.xml");
+
+            return xdoc.Descendants("recipe").Select(@node => new Recipe { Id = Guid.Parse(@node?.Attribute("id").Value), Title = @node?.Attribute("title").Value }).ToList();
+
+
+
+        }
+    }
+}
