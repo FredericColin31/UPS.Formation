b06e187f125f87409058686e7c694fb832e527ef
Frederic Colin <Frederic.Colin@equadex.net>
2023-02-17 15:24:33 +0100

    DB1RecipesService

---
+++ SolutionRecipe666/Services/Core/Config.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Core
+{
+    public static class Config
+    {
+        public static String? GetValueFrom(String key)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false);
+
+            IConfiguration config = builder.Build();
+
+            return config.GetSection(key)?.Value;
+        }
+    }
+}
---
+++ SolutionRecipe666/Services/DB1RecipesService.cs
@@ -0,0 +1,42 @@
+﻿using DataContracts;
+using Services.Core;
+using ServicesContracts;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class DB1RecipesService : AbstractRecipeService
+    {
+        public override List<Recipe> GetAll()
+        {
+            var recipes = new List<Recipe>();
+
+            using (var cn = new SqlConnection(Config.GetValueFrom("RecipesConnectionString")))
+            {
+                cn.Open();
+
+                var cmd = cn.CreateCommand();
+
+                cmd.CommandText = "SELECT * FROM Recipes";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var id = reader["id"].ToString();
+                    var title = reader["title"].ToString();
+
+                    recipes.Add(new Recipe() { Id=Guid.Parse(id), Title=title });
+                }
+
+                return recipes;
+            }
+        }
+    }
+}
