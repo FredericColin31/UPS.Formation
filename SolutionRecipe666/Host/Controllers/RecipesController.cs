0dd7e78eb783561303c6199f4b5d7c9647fdfbf1
Frederic Colin <Frederic.Colin@equadex.net>
2023-02-24 15:51:39 +0100

    RecipesController

---
+++ SolutionRecipe666/Host/Controllers/RecipesController.cs
@@ -0,0 +1,18 @@
+﻿using DataContracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace Host.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet("")]
+        public List<Recipe>? GetRecipes()
+        {
+            return Factory.Instance?.GetAll();
+        }
+    }
+}
