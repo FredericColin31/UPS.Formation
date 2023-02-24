48aba4a476d1fafbdf22de2b8a3a05d84fe55f7f
Frederic Colin <Frederic.Colin@equadex.net>
2023-02-24 14:27:12 +0100

    CDB3RecipesService ith prmetrized connection string in appsettings

--- SolutionRecipe666/Services/DataAccessLayer/BRecipesContext.cs
+++ SolutionRecipe666/Services/DataAccessLayer/BRecipesContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Services.Core;
 
 namespace Services.DataAccessLayer;
 
@@ -20,8 +21,7 @@ public partial class BRecipesContext : DbContext
     public virtual DbSet<Step> Steps { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=.\\SQL2019;Initial Catalog=bRecipes;Integrated security=SSPI;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer(Config.GetValueFrom("RecipesConnectionString"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
